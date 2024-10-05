4f2620fdab6b317a7f78b3b7fa62f421c4359bad
L65_USER ON WS03 <L65Y09W03@citulab.net>
2024-10-05 10:23:43 +0800

    forgotPass and controller

--- Accord/src/main/java/com/accord/controller/RegisterLoginController.java
+++ Accord/src/main/java/com/accord/controller/RegisterLoginController.java
@@ -5,9 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -89,5 +87,9 @@ public class RegisterLoginController {
     	public String showForgotPasswordPage() {
         return "forgotPassword_page"; // This must match the actual template name (without .html)
     	}
-
+		
+	@PostMapping("/forgotPassword_email")
+    	public String showForgotPasswordEmailPage() {
+        return "forgotPassword_email"; 
+    	}	
 }
