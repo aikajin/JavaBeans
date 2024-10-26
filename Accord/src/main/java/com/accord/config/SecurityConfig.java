b67c3badcb2beaa676eaf51fcdd9dee3213e94af
aikajin <138427546+aikajin@users.noreply.github.com>
2024-10-26 08:26:32 +0800

    Revert "Add area pls taronga ang html kay gi guba nako"

--- Accord/src/main/java/com/accord/config/SecurityConfig.java
+++ Accord/src/main/java/com/accord/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.accord.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,19 +11,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
-import com.accord.config.AuthSuccessHandlerImpl;
-import com.accord.config.AuthFailHandlerImpl;
-
 @Configuration
 public class SecurityConfig {
     
     @Autowired
-	private AuthSuccessHandlerImpl authSuccessHandlerImpl;
-    @Autowired
-    private AuthenticationSuccessHandler authenticationSuccessHandler;
-    @Autowired
-    @Lazy
-    private AuthFailHandlerImpl authFailHandlerImpl;
+	private AuthenticationSuccessHandler authenticationSuccessHandler;
 
 	@Bean
     public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
@@ -32,16 +23,13 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable())
             .cors(cors -> cors.disable())
             .authorizeRequests(req -> req
-                .requestMatchers("/css/**", "/images/**").permitAll()
-                .requestMatchers("/dashboard_user", "/manage-profile").hasRole("USER")
-                .requestMatchers("/dashboard_user", "/manage-profile").authenticated()
+                .requestMatchers("/dashboard_user").hasRole("USER")
                 .requestMatchers("/dashboard_admin").hasRole("ADMIN")
-                .requestMatchers("/", "/login", "/register", "/register_page_admin", "/forgotPassword_page").permitAll())
+                .requestMatchers("/").permitAll())
             .formLogin(form -> form
                 .loginPage("/").permitAll()
                 .loginProcessingUrl("/").permitAll()
-                .successHandler(authenticationSuccessHandler)
-                .failureHandler(authFailHandlerImpl))
+                .successHandler(authenticationSuccessHandler))
             .logout(logout -> logout
                 .logoutSuccessUrl("/").permitAll()
                 .deleteCookies("JSESSIONID").permitAll());
